20b92df202b80e780f435c8449a28dbeceb7aa6a
williemilio2 <emilio.gonzalezcanovas@gmail.com>
2026-05-22 00:14:26 +0200

    CREO que es esto

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ["@sparticuz/chromium"],
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
